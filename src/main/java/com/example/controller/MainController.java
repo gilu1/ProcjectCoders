c7365875b4e7a599ea0365666a4b0be5ecc86141
gilu1 <gilus362@gmail.com>
2018-01-25 10:50:07 +0100

    dodanie restController

--- src/main/java/com/example/controller/MainController.java
+++ src/main/java/com/example/controller/MainController.java
@@ -1,5 +1,10 @@
 package com.example.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value ="ss")
 public class MainController {
 
 }
