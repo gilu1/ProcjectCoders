d164c3830c59672f977d9382ef32527d332aeb17
gilu1 <gilus362@gmail.com>
2018-02-03 17:53:29 +0100

    dodanie serwisu

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -3,29 +3,42 @@ var app = angular.module('front-end', [ 'ui.router' ]);
 app.config(configFunction);
 app.controller("registerCtrl", registerController);
 app.controller("loginCtrl", loginController);
+app.service("mainService", mainService);
 
-app.controller('FirstCtrl', [ '$scope', function($scope) {
-	$scope.message = "świecie";
-} ]);
+// app.controller('FirstCtrl', [ '$scope', function($scope) {
+// $scope.message = "świecie";
+// } ]);
 
-function configFunction ($stateProvider) {
-    $stateProvider
-        .state('register',{
-            views: {
-                'mainView': {
-                    templateUrl: '/partial/register-template.html',
-                    controller: 'registerCtrl'
-                }
-            }
-        })
-        .state('login',{
-                views: {
-                    'loginView': {
-                        templateUrl: '/partial/login.html',
-                        controller: 'loginCtrl'
-                    },
-                    
-                }
-            })
+function configFunction($stateProvider) {
+	$stateProvider.state('register', {
+		views : {
+			'mainView' : {
+				templateUrl : '/partial/register-template.html',
+				controller : 'registerCtrl'
+			}
+		}
+	}).state('login', {
+		views : {
+			'loginView' : {
+				templateUrl : '/partial/login.html',
+				controller : 'loginCtrl'
+			},
+
+		}
+	})
 }
 
+function mainService($http) {
+	
+	return $http({
+		method : 'POST',
+		url : '/add/user',
+		params : {
+			email : user.email,
+			first_name : user.name,
+			last_name : user.lastname,
+			login : user.login,
+			password : user.password
+		}
+	})
+}
