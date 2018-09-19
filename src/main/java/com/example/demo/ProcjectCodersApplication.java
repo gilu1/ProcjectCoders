0a90cf79b81c11df90b4c48081c1819fa79149b4
Konrad <gilus362@gmail.com>
2018-09-19 10:43:07 +0200

    testDupy

--- src/main/java/com/example/demo/ProcjectCodersApplication.java
+++ src/main/java/com/example/demo/ProcjectCodersApplication.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan
 public class ProcjectCodersApplication {
 
+	//dupa
 	public static void main(String[] args) {
 		SpringApplication.run(ProcjectCodersApplication.class, args);
 	}
